a79ed8745131701cae676438ab68ba2a529805d9
YoSoyJesseFuerte <fortejesse@yahoo.com>
2018-03-02 18:16:14 -0700

    Map Car push with React

---
+++ src/Mapcar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+ 
+class Mapcar extends Component {
+  render() {
+    return (
+      <div className="helloContainer">
+        <h1>"Connecting Maps With Cars"</h1>
+	
+
+
+      </div>
+    );
+  }
+}
+
+ 
+export default Mapcar;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Mapcar from './Mapcar';
+import "./index.css";
+import './Mapcar.css';
+
+ReactDOM.render(
+    <Mapcar/>, 
+    document.getElementById('root')
+);
